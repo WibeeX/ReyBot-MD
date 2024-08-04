d3b5e8b22fd0a460bcaf7e617ddb2032788970f6
WibeeX <wilbertytjmz3@gmail.com>
2024-08-05 00:20:03 +0200

    Create grupo-add.js

---
+++ plugins/grupo-add.js
@@ -0,0 +1,23 @@
+
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+let who 
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
+else who = m.chat
+let name = await conn.getName(m.sender)	
+let user = global.db.data.users[who]
+if (!global.db.data.settings[conn.user.jid].restrict) return conn.reply(m.chat, `Las restricciones no estan activas en el bot.`, m) 
+if (!text) throw `Ingrese un numero valido para agregarlo, por ejemplo:\n*${usedPrefix + command}* 18293733257`
+if (text.includes('+')) throw  `Ingrese un numero valido para agregarlo, por ejemplo:\n*${usedPrefix + command}* 18293733257`
+let group = m.chat
+let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
+await conn.reply(text+'@s.whatsapp.net', `Holaaa, soy ReyBot-MD, un bot de WhatsApp creador por WILBERT, alguien te invito a unirte al grupo, te estamos esperando!!!!\n\n${link}`, m, {mentions: [m.sender]})
+m.reply(`✅ Link enviado a *@${who.split`@`[0]}*`) 
+}
+handler.help = ['add', '+'].map(v => v + ' número')
+handler.tags = ['group']
+handler.command = /^(add|agregar|invitar|invite|añadir|\+)$/i
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+handler.fail = null
+export default handler
