871c2156b2e045b562c067fae3132f9c65559bbf
WibeeX <wilbertytjmz3@gmail.com>
2024-08-05 00:45:19 +0200

    Create imagenes-memes.js

---
+++ plugins/imagenes-memes.js
@@ -0,0 +1,22 @@
+
+/*
+     ig : https://www.instagram.com/wibesinho_01/
+*/
+
+import hispamemes from 'hispamemes'
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+	
+const meme = hispamemes.meme()
+conn.sendFile(m.chat, meme, '', '', m)
+m.react('🤣')
+
+}
+handler.help = ['meme']
+handler.tags = ['imagene']
+handler.command = ['meme', 'memes']
+
+handler.diamond = true
+handler.register = true
+
+export default handler
