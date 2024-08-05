fab9a8b437d5a4003afcb18c2ed19e8e944d0159
WibeeX <wilbertytjmz3@gmail.com>
2024-08-05 14:54:08 +0200

    Create index.js

---
+++ view/index.js
@@ -0,0 +1,12 @@
+
+window.onload = () => {
+  const chat = document.querySelector('div.container-fluid');
+  function addMsg(obj) {
+    const html = document.createElement('span');
+    html.className = 'msg';
+    html.innerHTML = obj;
+    chat.appendChild(html);
+  }
+
+  window.onclick = () => addMsg(12);
+};
